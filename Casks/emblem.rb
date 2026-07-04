88399ec4e9f98b7380837b4bce261261fa2dbd0c
Kevin Lee <kevincorvallis@gmail.com>
2026-07-04 09:11:20 -0700

    emblem 0.2.2

--- Casks/emblem.rb
+++ Casks/emblem.rb
@@ -2,8 +2,8 @@
 # On each release: update version and sha256 (printed by the release workflow,
 # or `shasum -a 256` the GitHub release DMG), then copy to the tap repo.
 cask "emblem" do
-  version "0.2.1"
-  sha256 "a5bd08fbd15ee29ebfd048c11e3b892f6d38abb1b7b01fcacb4f7752b4df1aee"
+  version "0.2.2"
+  sha256 "4e95eee53cbe2e27cee9eadbf59a33d8872ea67ea8b89db2e059a6588747f947"
 
   url "https://github.com/kevincorvallis/Emblem/releases/download/v#{version}/Emblem-#{version}.dmg"
   name "Emblem"
